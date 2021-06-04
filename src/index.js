190af78ec18112b3dfabc1aebcf05f0d9b814025
Nimi-AJ <toluwanimi.ajike@stu.cu.edu.ng>
2021-06-04 15:40:52 +0100

    changed path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import client from '../src/database/dindex.js';
-import { getUsers, createUser, deleteUser, updateUser } from '../src/queries/qindex.js'
+import client from './database/dindex.js';
+import { getUsers, createUser, deleteUser, updateUser } from './queries/qindex.js'
 
 const app = express();
 const port = process.env.PORT || 8000;
